baa1221a2cf4d667f1e81e23716fca53377db3fb
Kevin Wallacy de Souza Maciel <kevinwall@ufrn.edu.br>
2018-05-22 11:11:32 -0300

    Construtor do node implementado

--- includes/list.h
+++ includes/list.h
@@ -84,12 +84,12 @@ namespace sc
 					Node* m_next;
 					Node* m_prev;
 
-					/*
+					
 					Node(const T & d = T(), Node* p = nullptr, Node* n = nullptr) : m_data(d), m_next(n), m_prev(p)
 					{
 						//Empty
 					}
-					*/
+					
 			};
 
 		private:
